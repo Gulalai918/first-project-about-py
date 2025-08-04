6f526c3bbff1ec9c3d4d7e39da65a4b35c146a9e
Gulalai <gulalaiikhan53@gmail.com>
2025-08-04 19:03:22 +0500

    python operators

--- project.py
+++ project.py
@@ -1,32 +1,100 @@
-name = "gulalai khan"
-age = "23"
+a = 23
+b = 30
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
 
-print(name)
-print(name.upper())
-print(name.lower())
-print(name.find("khan"))
-print(name.endswith("n"))
-print(name.startswith("I"))
-print(name.casefold())
-print(name.count("a"))
-print(name.center(30))
-print(name.encode())
-print(name.encode("utf7"))
-print(name.encode("utf-16"))
-print(name.format_map("khan"))
-print(name.isidentifier())
-print(name.islower())
-print(name.isupper())
-print(age.isspace())
-print(age.isnumeric())
-print(name.isalpha())
-print(age.isalnum())
-print(age.isalpha())
-print(age.isdecimal())
-print(age.isdigit())
-print(name.replace("a", "t"))
-print(name.title())
-print(name.index("a"))
-print(name.rfind("i"))
-print(name.__dir__())
-print(name.join("5344"))
+
+print(a)
+a += 3
+print(a)
+b += 8
+print(b)
+a -= 2
+print(a)
+b -= 5
+print(b)
+a *= 5
+print(a)
+b *= 99
+print (b)
+a /= 30
+print(a)
+b /= 48
+print(b)
+a %= 38
+print(a)
+b %= 46
+print(b)
+a **= 46
+print(a)
+b **= 76
+print(b)
+a //= 4
+print(a)
+b //= 33
+print(b)
+print(a := 79)
+print(a)
+a &= 60
+print(a)
+b : int = 9
+b &= 12
+print(b)
+a |= 22
+print(a)
+b |= 34
+print(b)
+a ^= 12
+print(a)
+b ^= 4
+print(b)
+a >>= 0
+print(a)
+b >>= 5
+print(b)
+a <<= 12
+print(a)
+b <<= 23
+print(b)
+
+
+x = 10
+y = 10
+print(x == y)
+print(x != y)
+print(x < y)
+print(x > y)
+print(x >= y)
+print(x <= y)
+
+
+print(x ==y and x != y)
+print(x > y or x == y)
+print(not (x < y and x == y))
+
+
+x : int = 45
+y :int = 45
+print(x is y)
+print(x is not y)
+
+
+x :str = ("Gualai", "naino", "qiela")
+print("naino" in x)
+print(not("nabelaj" in x))
+
+
+x = 5
+y = 9
+print(x & y)
+print(x | y)
+print(x ^ y)
+print(~ x)
+print(~ y)
+print(y << x)
+print(y >> x)
